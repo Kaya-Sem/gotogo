2c7d7068b10cae301b56d25063c557c8d92c3ca1
Kaya-Sem <kayasemvc@gmail.com>
2024-08-04 21:32:57 +0200

    refactored stying and printing of categories.

--- cmd/styles.go
+++ cmd/styles.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"sort"
+	"strings"
+	"time"
 
 	"github.com/charmbracelet/lipgloss"
 )
@@ -17,32 +20,67 @@ var checkedItemStyle = lipgloss.NewStyle().
 	Faint(true).
 	Strikethrough(true)
 
-var uncheckedIDStyle = lipgloss.NewStyle().
+var subtleTextStyle = lipgloss.NewStyle().
 	Italic(true).
 	Faint(true)
 
-var checkedIDStyle = lipgloss.NewStyle().
-	Italic(true).
-	Faint(true)
+var Title = lipgloss.NewStyle().
+	Underline(true)
 
 var checkedSymbolStyle = lipgloss.NewStyle().Foreground(lipgloss.Color(CheckedSymbolColor))
 
 func printFinishedItem(todo todoItem) {
 	idStr := fmt.Sprintf("%3d ", todo.ID) // Right-justified with a width of 3
-	fmt.Println(" " + checkedSymbolStyle.Render(CheckedSymbol) + checkedIDStyle.Render(idStr) + " " + checkedItemStyle.Render(todo.Title))
+	fmt.Println(" " + checkedSymbolStyle.Render(CheckedSymbol) + subtleTextStyle.Render(idStr) + " " + checkedItemStyle.Render(todo.Title))
 }
 
 func printUnfinishedItem(todo todoItem) {
 	idStr := fmt.Sprintf("%3d ", todo.ID) // Right-justified with a width of 3
-	fmt.Println(" " + UncheckedSymbol + uncheckedIDStyle.Render(idStr) + " " + todo.Title)
+	fmt.Println(" " + UncheckedSymbol + subtleTextStyle.Render(idStr) + " " + todo.Title)
+}
+
+func categorizeItemsByDay(items []todoItem) map[time.Time][]todoItem {
+	categorized := make(map[time.Time][]todoItem)
+
+	for _, item := range items {
+		// Truncate the timestamp to remove time information, keeping only the date
+		date := item.Timestamp.Truncate(24 * time.Hour)
+
+		// Append the item to the slice for this date
+		categorized[date] = append(categorized[date], item)
+	}
+
+	return categorized
 }
 
 func printItems(items []todoItem) {
-	for _, item := range items {
-		if item.Completed {
-			printFinishedItem(item)
-		} else {
-			printUnfinishedItem(item)
+	categorizedItemsMap := categorizeItemsByDay(items)
+
+	// Sort the days
+	var days []time.Time
+	for day := range categorizedItemsMap {
+		days = append(days, day)
+	}
+	sort.Slice(days, func(i, j int) bool {
+		return days[i].Before(days[j])
+	})
+
+	for _, day := range days {
+		categorizedItems := categorizedItemsMap[day]
+
+		// Format the day as a string and relative string
+		dayStr := subtleTextStyle.Render(day.Format("Monday, January 2"))
+		relativeDayStr := Title.Render(formatRelativeDate(day))
+
+		indent := strings.Repeat(" ", 1)
+		fmt.Printf("\n%s%s %s\n\n", indent, relativeDayStr, dayStr)
+
+		for _, item := range categorizedItems {
+			if item.Completed {
+				printFinishedItem(item)
+			} else {
+				printUnfinishedItem(item)
+			}
 		}
 	}
 }
