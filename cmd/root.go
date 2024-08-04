0cc48ba72368ffd7fecee9e92fc19751119c9e08
Kaya-Sem <kayasemvc@gmail.com>
2024-08-04 21:31:57 +0200

    added purgiq

--- cmd/root.go
+++ cmd/root.go
@@ -27,6 +27,7 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
+		purgeCompletedItems()
 		if len(args) == 0 {
 			printTodos()
 		} else {
